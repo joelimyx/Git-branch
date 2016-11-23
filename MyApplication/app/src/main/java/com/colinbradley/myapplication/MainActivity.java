ca8d869f9a892ebf895eb58575d9df0e11a5eabc
Joe Lim <joelimyx@gmail.com>
2016-11-23 10:49:34 -0500

    aded arraylist

--- MyApplication/app/src/main/java/com/colinbradley/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/colinbradley/myapplication/MainActivity.java
@@ -3,11 +3,16 @@ package com.colinbradley.myapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        List<Actor> actors = new ArrayList<>();
+        
     }
 }
