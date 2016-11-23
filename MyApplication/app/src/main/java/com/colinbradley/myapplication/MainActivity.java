e10f9e8f92895d51d019e47be238c94a2ff37516
cbrads61 <cbrads61@gmail.com>
2016-11-23 11:02:48 -0500

    .

--- MyApplication/app/src/main/java/com/colinbradley/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/colinbradley/myapplication/MainActivity.java
@@ -3,11 +3,20 @@ package com.colinbradley.myapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        List<Actor> actorList = new LinkedList<>();
+        actorList.add(1,new Actor("Johnny Depp", "idk", 7));
+        actorList.add(2, new Actor("Matt Damon", "some time in the past", 1));
+        actorList.add(3, new Actor("Bob, just Bob", "yesterday", 285));
     }
 }
