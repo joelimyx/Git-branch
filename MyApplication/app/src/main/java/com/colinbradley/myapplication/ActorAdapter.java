aceb7d8aa771d7177ebf258c2c2c989ced64ab51
Joe Lim <joelimyx@gmail.com>
2016-11-23 09:45:30 -0500

    Added actor class and adapter

---
+++ MyApplication/app/src/main/java/com/colinbradley/myapplication/ActorAdapter.java
@@ -0,0 +1,40 @@
+package com.colinbradley.myapplication;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.List;
+
+/**
+ * Created by Joe on 11/23/16.
+ */
+
+public class ActorAdapter extends RecyclerView.Adapter<ActorAdapter.ActorViewHolder>{
+    private List<Actor> mActors;
+
+    public ActorAdapter(List<Actor> actors) {
+        mActors = actors;
+    }
+
+    @Override
+    public ActorViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(ActorViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    class ActorViewHolder extends RecyclerView.ViewHolder{
+        public ActorViewHolder(View itemView) {
+            super(itemView);
+        }
+    }
+}
